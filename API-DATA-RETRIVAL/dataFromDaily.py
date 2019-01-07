4792a05437cc1cc2e2056c452ff036a0780691c1
Guymoz84m <guymozes@gmail.com>
2019-01-07 18:36:06 +0200

    Fixes after update the age gender table

--- API-DATA-RETRIVAL/dataFromDaily.py
+++ API-DATA-RETRIVAL/dataFromDaily.py
@@ -5,10 +5,26 @@ import requests
 INPUT_FILE = os.path.dirname(os.getcwd()) + "/API-DATA-RETRIVAL/daily_intake.csv"
 
 age_gender = set()
+nutrition = {
+	'sugar' : 1,
+	'calcium' : 2,
+	'sodium' : 3,
+	'protein' : 4,
+	'cholesterol' : 5,
+	'potassium' : 6,
+	'lactose' : 7,
+	'vitaminC' : 8,
+	'saturated' : 9,
+	'dietary_fiber' : 10,
+	'calories_kcal' : 11,
+	'magnesium' : 12,
+	'zinc' : 13,
+	'alcohol' : 14
+}
 
 age_gender_queries = ""
 # define sql queries
-add_age_gender = """INSERT INTO RECOMMEND_BY_AGE_GENDER (gender, age, protein_mg, cholesterol_mg_MAX, potassium_mg, lactose_mg, vitaminC_mg, saturated_fat_mg_MAX, dietary_fiber_mg, calories_kcal, alcohol_mg_MAX, magnesium_mg, zinc_mg) VALUES ('{}',{},{},{},{},{},{},{},{},{},{},{},{});"""
+add_age_gender = """INSERT INTO RECOMMEND_BY_AGE_GENDER (gender, age, nutrition_id, weight_mg) VALUES ('{}',{},{},{});"""
 
 with open(INPUT_FILE, 'r') as fin:
     reader = csv.reader(fin, lineterminator='\n')
@@ -17,24 +33,11 @@ with open(INPUT_FILE, 'r') as fin:
             continue
         gender = row[0]
         age = row[1]
-        protein_mg = row[2]
-        cholesterol_mg_MAX = row[3]
-        potassium_mg = row[4]
-        lactose_mg = row[5]
-        vitaminC_mg = row[6]
-        saturated_fat_mg_MAX = row[7]
-        dietary_fiber_mg = row[8]
-        calories_kcal = row[9]
-        alcohol_mg_MAX = row[10]
-        magnesium_mg = row[11]
-        zinc_mg = row[12]
-
-        if (gender, age, protein_mg, cholesterol_mg_MAX, potassium_mg, lactose_mg, vitaminC_mg, saturated_fat_mg_MAX, dietary_fiber_mg, calories_kcal, alcohol_mg_MAX, magnesium_mg, zinc_mg) in age_gender:
-            continue
-        else:
-            # adding new food to set
-            age_gender.add((gender, age, protein_mg, cholesterol_mg_MAX, potassium_mg, lactose_mg, vitaminC_mg, saturated_fat_mg_MAX, dietary_fiber_mg, calories_kcal, alcohol_mg_MAX, magnesium_mg, zinc_mg))
-            age_gender_queries += add_age_gender.format(gender, age, protein_mg, cholesterol_mg_MAX, potassium_mg, lactose_mg, vitaminC_mg, saturated_fat_mg_MAX, dietary_fiber_mg, calories_kcal, alcohol_mg_MAX, magnesium_mg, zinc_mg)
+        for nutrition_id in range(0,len(row[2:])):
+	        if (gender, age, nutrition_id, row[2+nutrition_id]) not in age_gender:
+	            # adding new food to set
+	            age_gender.add((gender, age, nutrition_id+1, row[2+nutrition_id]))
+	            age_gender_queries += add_age_gender.format(gender, age, nutrition_id+1, row[2+nutrition_id])
 
 recipes_sql2 = open('insert_age_gender.sql', 'w')
 recipes_sql2.write(age_gender_queries)

f03136d62c5ad8bfe5c812056e999ed9f74503e2
Guymoz84m <guymozes@gmail.com>
2019-01-08 18:48:26 +0200

    Removes API - for clean

--- API-DATA-RETRIVAL/connectionDetails.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SERVER_NAME = "127.0.0.1"
-SERVER_PORT = 3307
-DB_USERNAME = "DbMysql11"
-DB_PASSWORD = "DbMysql11"
-DB_NAME = "DbMysql11"
-PATH_ROOT = os.path.dirname(os.getcwd())
-print('The path root: ', PATH_ROOT)

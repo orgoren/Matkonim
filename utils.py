c75fc673d7ccae35f640f9b6f5f42bee605c10f4
Or Goren <orgoren@Ors-MacBook-Pro-5.local>
2019-01-12 18:22:42 +0200

    added age options

--- utils.py
+++ utils.py
@@ -14,15 +14,17 @@ NUTRITIONS = [	"sugar",   "iron",     "calcium", "sodium", "protein", "cholester
 				"lactose", "vitaminC", "saturated",  "dietary_fiber",  "alcohol", "calories"]
 
 MEAL_OPTIONS = ["Main Dishes", "Side Dishes", "Appetizers", "Lunch", "Breakfast and Brunch", 
-				"Snacks", "Soups", "Salads", "Breads", "Condiments and Sauce", "Desserts"]
+				"Snacks", "Soups", "Salads", "Breads", "Condiments and Sauce", "Desserts", "FullDay"]
 
 VALUES = {"None" : "None", "1" : "None", "d": "dont care", "2" : "less than 5%", "3" : "over 30%"}
 
+AGE_RANGES = {"14_18" : "0", "19_30" : "1", "31_40" : "2", "41_50" : 3, "51_60" : 4, "61_70" : 5, "71" : 6}
 
 def get_username_and_password():
 	username = raw_input("enter username (for nova): ")
 	password = getpass.getpass("enter password (for nova): ")
 	return username, password
+
 
 def get_nutritions_values(form, is_food=True):
 	nutritions_values = {}
@@ -30,12 +32,14 @@ def get_nutritions_values(form, is_food=True):
 	for nutrition in NUTRITIONS:
 		if is_food and nutrition == "alcohol":
 			continue
+
 		nutritions_values[nutrition] = str(form.get(nutrition))
 		if nutritions_values[nutrition] == "":
 			nutritions_values[nutrition] = "d"
 		if nutrition == "calories" and nutritions_values[nutrition] == "None":
 			nutritions_values[nutrition] = "d"
 	return nutritions_values
+
 
 def get_meal_option(form):	
 	for mo in MEAL_OPTIONS:

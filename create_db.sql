1ab7193a1e95f58052c81ea881a9422d3181a720
Guymoz84 <guymozes@gmail.com>
2019-01-14 23:00:29 +0200

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -4,23 +4,28 @@ USE DbMysql11;
 
 -- DROP TABLE ALL_RECIPES
 CREATE TABLE DbMysql11.ALL_RECIPES(
-	recipe_id SMALLINT UNSIGNED NOT NULL,
-  	recipe_name VARCHAR(255) NULL DEFAULT NULL,  
-  	picture VARCHAR(255),
-  	PRIMARY KEY (recipe_id),
-  	INDEX recipe_index (recipe_id ASC))
+	recipe_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	recipe_name VARCHAR(255) NULL DEFAULT NULL,  
+	picture VARCHAR(255),
+	PRIMARY KEY (recipe_id),
+	INDEX recipe_index_ar (recipe_id ASC))
 ENGINE = InnoDB
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4;
 
 -- DROP TABLE FOOD_RECIPES
 CREATE TABLE DbMysql11.FOOD_RECIPES(
-	recipe_id SMALLINT UNSIGNED NOT NULL,
+	recipe_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	food_id INT UNSIGNED NOT NULL,
-  	course VARCHAR(20) NOT NULL,
-  	prep_time_in_minutes SMALLINT UNSIGNED NOT NULL,
-  	food_details VARCHAR(2500) NOT NULL,
-  	INDEX food_id (food_id ASC)) 
+	course VARCHAR(27) NOT NULL,
+	prep_time_in_minutes SMALLINT UNSIGNED NOT NULL,
+	food_details VARCHAR(2500) NOT NULL,
+	PRIMARY KEY (recipe_id, food_id),
+	INDEX food_id (food_id ASC),
+	INDEX recipe_id_index_food_rec (recipe_id ASC),
+	INDEX course (course)) 
 ENGINE = InnoDB
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4;
 
 -- DROP TABLE COCKTAIL_RECIPES
@@ -30,11 +35,13 @@ CREATE TABLE DbMysql11.COCKTAIL_RECIPES (
   	is_alcoholic TINYINT UNSIGNED NOT NULL,
   	cocktail_details VARCHAR(3500) NOT NULL,
 	serving_glass VARCHAR(100) DEFAULT NULL,
-  	INDEX cocktail_id (cocktail_id ASC))
+	PRIMARY KEY (cocktail_id),
+  	INDEX cocktail_id (cocktail_id ASC),
+	INDEX recipe_id_index_cocktail (recipe_id ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
-
+-- DROP TABLE DbMysql11.INGREDIENTS
 CREATE TABLE DbMysql11.INGREDIENTS (
   	ingredient_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   	ingredient_name VARCHAR(55) NOT NULL,
@@ -43,7 +50,7 @@ CREATE TABLE DbMysql11.INGREDIENTS (
   	serving_weight_grams SMALLINT UNSIGNED NOT NULL,
   	PRIMARY KEY (ingredient_id),
   	INDEX ingredient_id (ingredient_id ASC),
-  	FULLTEXT INDEX ingredientName (ingredient_name) )
+  	FULLTEXT INDEX ingredient_name_index (ingredient_name) )
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4;
@@ -54,29 +61,33 @@ CREATE TABLE DbMysql11.RECIPE2INGREDIENTS(
   	ingredient_id INT UNSIGNED NOT NULL,
   	servings FLOAT(7) UNSIGNED NOT NULL,
   	full_ingredient_line VARCHAR(2500) NOT NULL,
-  	INDEX (recipe_id))
+  	INDEX recipe_id (recipe_id ASC),
+   INDEX ingredient_id_rec2ing (ingredient_id ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
 
+-- DROP TABLE DbMysql11.INGREDIENT_NUTRITION
 CREATE TABLE DbMysql11.INGREDIENT_NUTRITION(
 	ingredient_id INT UNSIGNED NOT NULL,
 	nutrition_id SMALLINT UNSIGNED NOT NULL,
 	weight_mg_from_ingredient Float(3) DEFAULT 0,
-   INDEX (ingredient_id),
+	PRIMARY KEY (ingredient_id, nutrition_id),
+	INDEX ingredient_id_ingred_nutr (ingredient_id ASC),
 	FOREIGN KEY (ingredient_id)
 	REFERENCES DbMysql11.INGREDIENTS (ingredient_id)
 	ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
-DROP TABLE NUTRITIONS
+-- DROP TABLE NUTRITIONS
 CREATE TABLE DbMysql11.NUTRITIONS (
 	nutrition_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	nutrition_name VARCHAR(30) NOT NULL,
 	max_or_min VARCHAR(7) NOT NULL,
 	PRIMARY KEY (nutrition_id),
-	INDEX(nutrition_id))
+	INDEX nut_id_index (nutrition_id ASC),
+	INDEX nut_name_index (nutrition_name))
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4;
@@ -88,7 +99,9 @@ CREATE TABLE DbMysql11.RECOMMEND_BY_AGE_GENDER(
 	age TINYINT NOT NULL, -- 0: 14-18, 1:19-30, 2: 31-40, 3: 41-50, 4:51-60, 5:61-70, 6: 71+
 	nutrition_id SMALLINT UNSIGNED NOT NULL,
 	weight_mg Float(3) DEFAULT 0,
-   PRIMARY KEY (gender, age, nutrition_id),
-   INDEX (gender, age, nutrition_id))
+	PRIMARY KEY (gender, age, nutrition_id),
+	INDEX nut_id_index_recom_gender_age (nutrition_id),
+	INDEX (gender, age),
+	INDEX weight_mg_index_recom_gender_age (weight_mg ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
